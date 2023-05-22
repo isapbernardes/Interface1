399ed9ade01a2a0e1d422e725f177f45b2fe41fb
ISADORA BERNARDES <88350340+isapbernardes@users.noreply.github.com>
2023-05-22 15:36:49 -0300

    animate

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.scss'
+import { Logo } from './components/logo'
+
+function App() {
+  return (
+   <main>
+    <header className='header-container'>
+      <Logo/>
+      <nav className="menu-options">
+        <a>What we offer</a>
+        <a>How it works</a>
+        <a>Portfólio</a>
+      </nav>
+      <button>
+        <div className='btn-face-white'>
+          Contact us
+          </div>
+          <div className='btn-face-purple'>
+            Contact us
+         </div>
+       </button>
+     </header>
+      <div className="content-container">
+           <div className='heading-container'>
+           <h2>Lets grow your</h2>
+           <h2>social presence.</h2>
+           </div>
+         <div className="bubble target">
+         🎯
+         </div>
+         <div className="bubble shop">
+         🛍️
+         </div>
+         <div className="bubble rocket">
+         🚀
+         </div>
+         <div className="bubble fire">
+         🔥
+         </div>
+      </div>
+    </main>
+  )
+}
+
+
+export default App
